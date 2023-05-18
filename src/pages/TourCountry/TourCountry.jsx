945c6cc10cb5c31467898bb4524152506a08ed39
EZ_Chung <anhchung546@gmail.com>
2023-05-18 09:50:40 +0700

    them cho vui

--- src/pages/TourCountry/TourCountry.jsx
+++ src/pages/TourCountry/TourCountry.jsx
@@ -44,6 +44,7 @@ export default function TourCountry() {
           location: "Đà Nẵng",
           totalDays: 3,
           dateStart: "22/03/2023",
+          dateGo: "",
           slots: 10,
           prices: 3000000,
           desc: "Du lịch Đà Nẵng - Huế - Thánh Địa La Vang - Động Phong Phong Nha từ Sài Gòn 2023. Du lịch Miền Trung - Tour Du lịch Đà Nẵng được thiên nhiên đặc biệt ưu đãi, mảnh đất miền Trung đẹp với nhiều dãy núi hùng vỹ xanh rì, những bờ biển cát trắng mịn thoai thoải và những dòng sông trong vắt thơ mộng. Không những vậy, trên con đường di sản miền Trung cùng Du Lịch Việt, du khách còn được thưởng ngoạn những di sản thế giới cuả Việt Nam đó là Phố cổ Hội An – nơi bến cảng một thời sầm uất nhất Đông Dương, quần thể di tích Cố Đô Huế với hệ thống đền đài lăng tẩm nguy nga tráng lệ và Động Phong Nha với nhiều hang động kì bí của tạo hóa.",
@@ -54,7 +55,8 @@ export default function TourCountry() {
         dataTransferSouth.title = element.nameTour;
         dataTransferSouth.location = element.placeDest;
         dataTransferSouth.totalDays = 3;//dateBack-dateGo
-        dataTransferSouth.dateStart = element.dateGo;//dateGo
+        dataTransferSouth.dateStart = element.dateGo;
+        dataTransferSouth.dateGo = element.dateGo;//dateGo
         //desc = note
         dataTransferSouth.desc = element.note;
         dataTransferSouth.slots = element.adultSlot + element.childrenSlot;
@@ -153,6 +155,7 @@ export default function TourCountry() {
         dataTransferCentral.location = element.placeDest;
         dataTransferCentral.totalDays = 3;//dateBack-dateGo
         dataTransferCentral.dateStart = element.dateGo;//dateGo
+        dataTransferCentral.dateGo = element.dateGo;
         //desc = note
         dataTransferCentral.desc = element.note;
         dataTransferCentral.slots = element.adultSlot + element.childrenSlot;
